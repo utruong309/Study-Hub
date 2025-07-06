f2ad5cd73a27d6d8c5e06cc4bd62181ecb0ba3a0
utruong309 <uyenthutruong09@gmail.com>
2025-07-06 18:48:48 +0700

    fix: add .env to gitignore

--- client/src/config.js
+++ client/src/config.js
@@ -7,15 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDlQrerSvuHi2THBABxUkQTsu8vpw-6Mdk",
-  authDomain: "studyhub-dfd67.firebaseapp.com",
-  projectId: "studyhub-dfd67",
-  storageBucket: "studyhub-dfd67.firebasestorage.app",
-  messagingSenderId: "967834634580",
-  appId: "1:967834634580:web:5d0d62d79e99ba3147a0c1",
-  measurementId: "G-2G0PFPMF98"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export default app; 
