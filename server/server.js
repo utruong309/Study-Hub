24ad782c1f6f1ee1518898731e06b8eb0b8e1974
utruong309 <uyenthutruong09@gmail.com>
2025-07-07 12:14:27 +0700

    add firebase secrey key

---
+++ server/server.js
@@ -0,0 +1,23 @@
+import express from "express"; 
+import mongoose from "mongoose"; 
+import cors from "cors"; 
+import dotenv from "dotenv";
+import noteRoutes from "./routes/notes"; 
+
+dotenv.config(); 
+
+const app = express(); 
+app.use(cors()); 
+app.use(express.json()); 
+
+app.use("/api/notes", noteRoutes); //define name of routes
+
+//Connect to MongoDB
+mongoose    
+    .connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => 
+            console.log(`Server running on port ${process.env.PORT}`)
+        ); 
+    })
+    .catch((err) => console.error("MongoDB connection error:", err)); 
